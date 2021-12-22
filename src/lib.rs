64727ba499fbbf0de2aa57c86aad122b284a21d7
Alfie John <alfie@alfie.wtf>
2021-12-22 10:49:38 +0000

    Moving from ramp to num-bigint for Big Numbers

--- src/lib.rs
+++ src/lib.rs
@@ -37,9 +37,11 @@
 
 /// This code was translated from https://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test#Perl
 
-extern crate ramp;
+extern crate num_bigint;
+extern crate num_traits;
 
-use ramp::int::Int;
+use num_bigint::BigUint;
+use std::str::FromStr;
 
 /// Tests if the given integer within a string is prime
 ///
@@ -96,84 +98,79 @@ pub fn is_prime(n_str: &str) -> bool {
 ///}
 ///```
 pub fn is_prime_with_witnesses(n_str: &str, witnesses: usize) -> bool {
-    let two        = Int::from_str_radix("2", 10).unwrap();
-    let n          = Int::from_str_radix(n_str, 10).unwrap();
-    let n_sub: Int = n.clone() - 1;
+    let n = BigUint::from_str(n_str).unwrap();
 
-    if n == 2 || n == 3 || n == 5 {
-       return true;
+    if n == BigUint::from(2u32) || n == BigUint::from(3u32) || n == BigUint::from(5u32) {
+        return true
     }
 
-    if n < 2 || (n.divmod(&two).1 == 0) {
-       return false;
+    if n < BigUint::from(2u32) {
+        return false;
     }
 
+    if (&n % BigUint::from(2u32)) == BigUint::from(0u32) {
+        return false;
+    }
+
+    let n_sub: BigUint = n.clone() - BigUint::from(1u32);
     let mut exponent = n_sub.clone();
-    let mut trials   = Int::zero();
+    let mut trials   = 0;
 
-    while exponent.divmod(&two).1 == 0 {
-        exponent = exponent / 2;
-        trials   = trials + 1;
+    while (&exponent % BigUint::from(2u32)) == BigUint::from(0u32) {
+        exponent = exponent / 2u32;
+        trials  += 1;
     }
 
     'LOOP: for i in 1..(witnesses + 1) {
-        let mut result = powmod(&(two.clone() + i), &exponent, &n);
+        let mut result = bmodpow(&(BigUint::from(2u32) + i), &exponent, &n);
 
-        if result == 1 || result == n_sub {
+        if result == BigUint::from(1u32) || result == n_sub {
             continue;
         }
 
-        let mut verified = 1;
+        for _ in 1 .. trials {
+            result = result.pow(2) % &n;
 
-        while verified < trials {
-            result = result.square().divmod(&n).1;
-
-            if result == 1 {
-                return false;
+            if result == BigUint::from(1u32) {
+                return false
             }
 
             if result == n_sub {
                 continue 'LOOP;
             }
-
-            verified = verified + 1;
         }
 
         return false;
     }
 
-    return true
+    true
 }
 
 // translated from http://search.cpan.org/~pjacklam/Math-BigInt-1.999810/lib/Math/BigInt.pm#Arithmetic_methods
-fn powmod(base: &Int, exponent: &Int, modulus: &Int) -> Int {
-    if *base == Int::zero() {
-        return match *exponent == Int::zero() {
-            true  => Int::one(),
-            false => Int::zero(),
+fn bmodpow(base: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
+    if *base == BigUint::from(0u32) {
+        return match *exponent == BigUint::from(0u32) {
+            true => BigUint::from(1u32),
+            false => BigUint::from(0u32),
         }
     }
 
-    if *modulus == Int::one() {
-        return Int::zero();
+    if *modulus == BigUint::from(1u32) {
+        return BigUint::from(0u32);
     }
 
-    let exponent_in_binary      = Int::to_str_radix(&exponent, 2, false);
-    let mut exponent_chars_revd = exponent_in_binary.chars().rev();
-    let mut exponent_length     = exponent_in_binary.len();
-    let mut my_base             = base.clone();
-    let mut result              = Int::one();
+    let exponent_in_binary = exponent.to_radix_le(2);
+    let mut my_base = base.clone();
+    let mut result  = BigUint::from(1u32);
 
-    while exponent_length > 0 {
-        exponent_length = exponent_length - 1;
-
-        if exponent_chars_revd.next().unwrap() == '1' {
+    for next_bit in exponent_in_binary {
+        if next_bit == 1 {
             result = result * my_base.clone();
-            result = result.divmod(&modulus).1;
+            result = result % modulus;
         }
 
-        my_base = my_base.square();
-        my_base = my_base.divmod(&modulus).1;
+        my_base = my_base.pow(2);
+        my_base = my_base % modulus;
     }
 
     result
